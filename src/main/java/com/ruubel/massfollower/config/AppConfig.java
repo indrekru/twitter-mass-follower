50f0e2abdc3eee2a1264af5652dc06e247d33e84
Indrek Ruubel <indrekruubel@gmail.com>
2019-07-28 17:47:55 +0300

    add rollbar

--- src/main/java/com/ruubel/massfollower/config/AppConfig.java
+++ src/main/java/com/ruubel/massfollower/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.ruubel.massfollower.config;
 
+import com.rollbar.notifier.Rollbar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.TimeZone;
+
+import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
 @Configuration
 public class AppConfig {
@@ -20,6 +23,12 @@ public class AppConfig {
     @Bean
     public ConfigParams configParams() throws Exception {
         return new ConfigParams();
+    }
+
+    @Bean
+    public Rollbar rollbar() {
+        Rollbar rollbar = Rollbar.init(withAccessToken("34cea2fc2be749e7a2ec60d066286f9f").build());
+        return rollbar;
     }
 
 }
